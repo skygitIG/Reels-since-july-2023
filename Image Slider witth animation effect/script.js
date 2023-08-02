d66ceb988f7bf0cb774163743dc11405c3879846
Akash Kumar <117715724+skygitIG@users.noreply.github.com>
2023-08-02 17:25:23 +0530

    Create script.js

---
+++ Image Slider witth animation effect/script.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "1",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 4,
+    slideShadows: false
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2
+    }
+  }
+});
