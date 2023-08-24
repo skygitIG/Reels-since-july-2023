9708cc51bfff9f7bba940c031827197b3058629f
Akash Kumar <117715724+skygitIG@users.noreply.github.com>
2023-08-24 16:44:51 +0530

    Create script.js

---
+++ Image sider with color filter/script.js
@@ -0,0 +1,4 @@
+const container = document.querySelector('.container');
+document.querySelector('.slider').addEventListener('input', (e) => {
+  container.style.setProperty('--position', `${e.target.value}%`);
+})
